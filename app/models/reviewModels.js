cc5ca11fb08d77eb26096c7febae7e6f94dfca95
H3nry-T <ht3drex@gmail.com>
2023-01-16 15:20:45 +0000

    edited LEFT JOIN and GROUP BY in the sql query (reviewModels)

--- app/models/reviewModels.js
+++ app/models/reviewModels.js
@@ -4,9 +4,9 @@ const fetchAllReviews = () => {
   const selectQuery = `
         SELECT reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, designer, COUNT(comment_id) AS comment_count 
         FROM reviews 
-        JOIN comments 
+        LEFT JOIN comments 
         ON reviews.review_id = comments.review_id
-        GROUP BY reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, designer
+        GROUP BY reviews.review_id
         ORDER BY reviews.created_at;
     `;
   return db.query(selectQuery).then((result) => {
--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 SELECT reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, designer, COUNT(comment_id) AS comment_count 
 FROM reviews 
-JOIN comments 
+LEFT JOIN comments 
 ON reviews.review_id = comments.review_id
-GROUP BY reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes
+GROUP BY reviews.review_id
 ORDER BY reviews.created_at;
 
 -- reviews.owner
