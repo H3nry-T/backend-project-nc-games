e0454a25ad0ea7514a73d497d170dfde01b4a06d
H3nry-T <ht3drex@gmail.com>
2023-01-20 10:55:14 +0000

    refactored all errors into another file

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const {
+  handleManualNotFoundErrors,
+  handleBadRequestErrors,
+  handleNotFoundErrors,
+  handleInternalServerErrors,
+  handleNoContentErrors,
+  handleCustomErrors,
+} = require("./errors/errors");
 const apiRouter = require("./routes/apiRouter");
 
 const app = express();
@@ -7,74 +15,21 @@ app.use(express.json());
 
 app.use("/api", apiRouter);
 
-// 204 no content error
-app.use((error, request, response, next) => {
-  if (error.code === 204) {
-    // console.log(error);
-    response.status(204).send({ error: error });
-  } else {
-    next(error);
-  }
-});
+// 204
+app.use(handleNoContentErrors);
 
-//route found MANUAL 404 Not found error
-app.use((error, request, response, next) => {
-  if (error.code === 404) {
-    // console.log(error);
-    response.status(404).send({ error: error });
-  } else {
-    next(error);
-  }
-});
+//404 MANUAL
+app.use(handleManualNotFoundErrors);
 
-//SQL POST/PATCH 400 bad request error
-app.use((error, request, response, next) => {
-  if (
-    (error.code =
-      400 ||
-      error.code === "22P02" ||
-      error.code === "23503" ||
-      error.code === "23502")
-  ) {
-    // console.log(error);
-    if (error.hasOwnProperty("msg")) {
-      //SQL custom errors
-      response.status(400).send({ error: error });
-    } else if (error.routine === "ExecConstraints") {
-      //SQL cannot insert null values
-      response.status(400).send({
-        error: {
-          code: 400,
-          msg: "Bad request must have username, body keys",
-        },
-      });
-    } else if (error.routine === "ri_ReportViolation") {
-      //SQL violates foreign key
-      response.status(400).send({
-        error: {
-          code: 400,
-          msg: "Bad request the username is not registered with us",
-        },
-      });
-    } else {
-      //SQL any other error
-      response.status(400).send({ error: { code: 400, msg: "Bad request" } });
-    }
-  } else {
-    next(error);
-  }
-});
+//400
+app.use(handleBadRequestErrors);
 
-//route not found 404 error
-app.use((request, response, next) => {
-  response.status(404).send({ code: 404, msg: "not found" });
-});
+//CUSTOM ERRORS
+app.use(handleCustomErrors);
 
-//500 internal server error
-app.use((error, request, response, next) => {
-  if (error) {
-    // console.log(error);
-    response.status(500).send({ error: error });
-  }
-});
+//404 DEFAULT
+app.use(handleNotFoundErrors);
+
+//500
+app.use(handleInternalServerErrors);
 module.exports = app;
---
+++ app/errors/errors.js
@@ -0,0 +1,77 @@
+//204
+exports.handleNoContentErrors = (error, request, response, next) => {
+  if (error.code === 204) {
+    // console.log(error);
+    response.status(204).send({ error: error });
+  } else {
+    next(error);
+  }
+};
+
+//404
+exports.handleManualNotFoundErrors = (error, request, response, next) => {
+  if (error.code === 404) {
+    // console.log(error);
+    response.status(404).send({ error: error });
+  } else {
+    next(error);
+  }
+};
+
+//400
+exports.handleBadRequestErrors = (error, request, response, next) => {
+  if (
+    (error.code =
+      400 ||
+      error.code === "22P02" ||
+      error.code === "23503" ||
+      error.code === "23502")
+  ) {
+    // console.log(error);
+    if (error.hasOwnProperty("msg")) {
+      //SQL custom errors
+      response.status(400).send({ error: error });
+    } else if (error.routine === "ExecConstraints") {
+      //SQL cannot insert null values
+      response.status(400).send({
+        error: {
+          code: 400,
+          msg: "Bad request must have username, body keys",
+        },
+      });
+    } else if (error.routine === "ri_ReportViolation") {
+      //SQL violates foreign key
+      response.status(400).send({
+        error: {
+          code: 400,
+          msg: "Bad request the username is not registered with us",
+        },
+      });
+    } else {
+      //SQL any other error
+      response.status(400).send({ error: { code: 400, msg: "Bad request" } });
+    }
+  } else {
+    next(error);
+  }
+};
+
+//custom errors
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.code && err.msg) {
+    res.status(err.status).send({ error: error });
+  } else next(err);
+};
+
+//404
+exports.handleNotFoundErrors = (request, response, next) => {
+  response.status(404).send({ code: 404, msg: "not found" });
+};
+
+//500
+exports.handleInternalServerErrors = (error, request, response, next) => {
+  if (error) {
+    // console.log(error);
+    response.status(500).send({ error: error });
+  }
+};
